8bc1986d2aca484bf458646eede6731542f5c59c
santosh jha <santosh0954jha@gmail.com>
2024-03-28 16:52:09 +0530

    home page design complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 console.log(
   'Designed with ❤ by "Santosh Jha". (Email: santoshjhavns@gmail.com)'
 );
+
+
+const toggleSearch = document.getElementById("toggle-search");
+const searchInput = document.getElementById("search-input");
+const navbar = document.querySelector(".menu");
+
+toggleSearch.addEventListener("click", function () {
+  searchInput.classList.toggle("d-none");
+  if (!searchInput.classList.contains("d-none")) {
+    toggleSearch.innerHTML = "<i class='fa fa-close'></i>";
+    navbar.classList.toggle("hideMenu");
+  } else {
+    toggleSearch.innerHTML = "<i class='fa fa-search'></i>";
+    navbar.classList.toggle("hideMenu");
+  }
+});
